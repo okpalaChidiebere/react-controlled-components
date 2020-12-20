5f9891a85245992095659b0ea83a3999671b1899
Chidiebere Okpala <okpalacollins4@gmail.com>
2020-12-20 16:23:41 -0600

    Controlled Components - 1
    Facts about Controlled Components
    - Each update to state has an associated handler function
    - Event handlers for a controlled element update the component's state
    - Form elements receive their current value via an attribute. Nothing about the normal form elements you know for html dom changes, it is just that now React is controlling the state of the form. This is why it is called Controlled component
    - Form input values are generally stored in the component's state

--- src/App.js
+++ src/App.js
@@ -3,6 +3,22 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    query: '',  //we will bind our input field to to whatever the value of a certain property of a state is
+  }
+
+  /**
+   * 
+   * Each update to state has an associated handler function. In this case we need only one handler.
+   * Event handlers for a controlled element update the component's state
+   */
+  handleChange = (event) => { //**the query argument will be passed event.target.value which is the value of the input field if we run an arrow function for the onchange event for the input box.
+    this.setState(() => ({
+      query: event.target.value.trim() //event.target.value is the dom attribute that we get this iput value from. Nothing about the normal form elements you know for html dom changes, it is just that now React is controlling the state of the form. This is why it is called Controlled component
+    }))
+  }
+
   render() {
     return (
       <div className="App">
@@ -11,9 +27,13 @@ class App extends Component {
           <h1 className="App-title">ReactND - Coding Practice</h1>
         </header>
         <div className="container">
-          <input type="text" placeholder="Say Something" />
+          <input type="text" 
+          placeholder="Say Something" 
+          value={this.state.query}
+          onChange={//(event) => this.handleChange(event.target.value)} writing it this way we will have to write more code
+            this.handleChange}/>
           <p className="echo">Echo:</p>
-          <p>This should mirror the text you typed into the input field.</p>
+          <p>{this.state.query}</p>
         </div>
       </div>
     );
